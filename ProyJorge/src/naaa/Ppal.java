09bbd8c4272a788df3ab47b8b1387d6de88a777d
Jorge Alonso <jorgealonso7@outlook.es>
2020-02-03 17:17:19 +0100

    fin syso

--- ProyJorge/src/naaa/Ppal.java
+++ ProyJorge/src/naaa/Ppal.java
@@ -2,6 +2,7 @@ package naaa;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Random;
 
 public class Ppal {
 
@@ -9,7 +10,12 @@ public class Ppal {
 		// TODO Auto-generated method stub
 
 		
-		String listaDNI[] = new String [3000];
+		Random r = new Random();
+//		int valorDado	=	r.nextInt(Max-min)+min;		
+		
+//		String listaDNI[] = new String [3000];
+		
+		String listaDNI[] = {"111A","222B"};
 		
 		// un array de cada habitacion del precio --> reserv 1 => precio 1
 		Double precioH[] = {45.21,105.12};
@@ -31,8 +37,16 @@ public class Ppal {
 //		System.out.println(salida);
 		
 		LocalDate entrada = LocalDate.of(2020, Month.JANUARY, 1);
-		
-		System.out.println("");
+		LocalDate salida = entrada.plusDays(2);
+//		INSERT INTO TEA_EXHIBE VALUES (1, TO_DATE('2020-01-29', 'YYYY-MM-DD'), 2);
+		/*
+		 * INSERT INTO HOT_RESERVA (ID_RESERVA, DNI_CLIENTE, ID_HABITACION, CHECK_IN, CHECK_OUT, PRECIO_HAB_DIA, PRECIO_SERV_DIA, PRECIO_TOTAL) 
+		 * VALUES ();
+		 * 
+		 * */
+		 
+		System.out.println("INSERT INTO HOT_RESERVA (ID_RESERVA, DNI_CLIENTE, ID_HABITACION, CHECK_IN, CHECK_OUT, PRECIO_HAB_DIA, PRECIO_SERV_DIA, PRECIO_TOTAL) \r\n" + 
+				"VALUES ("+ 1 +",'"+ listaDNI[0] +"'," + 1 +","+ entrada +","+ salida +"," + precioH[0] + "," + s1.getPrecio() + "," + (precioH[0]+s1.getPrecio()) +");");
 		
 		
 		
