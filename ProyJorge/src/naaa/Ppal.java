2ec6a58a602bffa63027254c3646db699bba12a7
Jorge Alonso <jorgealonso7@outlook.es>
2020-02-03 13:39:06 +0100

    naaaaaaaaaaaa

---
+++ ProyJorge/src/naaa/Ppal.java
@@ -0,0 +1,43 @@
+package naaa;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Ppal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String listaDNI[] = new String [3000];
+		
+		// un array de cada habitacion del precio --> reserv 1 => precio 1
+		Double precioH[] = {45.21,105.12};
+		//Habit hab1 = new Habit(1, 52.64);
+		//Habit hab2 = new Habit(2, 105.17);
+		Servic s1 = new Servic(55);
+		Servic s2 = new Servic(105);
+		
+//		LocalDate entrada = LocalDate.of(2020, Month.JANUARY, 1);
+//		LocalDate salida = entrada.plusDays(2);
+//		System.out.println(entrada);
+//		System.out.println(salida);
+//		
+//		entrada = salida.plusDays(2);
+//		
+//		System.out.println(entrada);
+//		
+//		salida = entrada.plusDays(2);
+//		System.out.println(salida);
+		
+		LocalDate entrada = LocalDate.of(2020, Month.JANUARY, 1);
+		
+		System.out.println("");
+		
+		
+		
+		
+		
+	}
+
+}
