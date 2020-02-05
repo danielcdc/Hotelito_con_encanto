220cdfc18ddd9e3c171b743ad301c9c12c30cee4
Jorge Alonso <jorgealonso7@outlook.es>
2020-02-05 09:14:30 +0100

    camb

--- ProyJorge/src/naaa/Ppal.java
+++ ProyJorge/src/naaa/Ppal.java
@@ -132,11 +132,11 @@ public class Ppal {
 			LocalDate salida = entrada.plusDays(ale);
 			
 			
-			precio=pack.generarServicios(aleServ, listaCliente[aleC], precio);
+			precio=pack.generarServicios(aleServ, i, precio);
 			
 			
-			System.out.print("INSERT INTO HOT_RESERVA (ID_RESERVA| DNI_CLIENTE| ID_HABITACION| CHECK_IN| CHECK_OUT| PRECIO_HAB_DIA| PRECIO_SERV_DIA| PRECIO_TOTAL) \r\n" + 
-					"VALUES ("+ listaCliente[aleC] +"| '"+ listaDNI[1] +"'| " + 10 +"| TO_DATE('"+ entrada 
+			System.out.print("INSERT INTO HOT_RESERVA (ID_RESERVA| ID_CLIENTE| ID_HABITACION| CHECK_IN| CHECK_OUT| PRECIO_HAB_DIA| PRECIO_SERV_DIA| PRECIO_TOTAL) \r\n" + 
+					"VALUES ("+ i +"| "+ listaCliente[aleC] +"| " + 10 +"| TO_DATE('"+ entrada 
 					+" 15:00'| 'YYYY-MM-DD HH24:MI')| TO_DATE('"+ salida +" 12:00'| 'YYYY-MM-DD HH24:MI')| " + pHab10[i] + "| " + precio );
 			System.out.printf("| %.2f);\n",((pHab1[i]+precio)*ale));
 			
